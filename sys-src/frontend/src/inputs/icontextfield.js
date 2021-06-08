dc4fd8a1dce42ee67577a642a4e95b559e8a61c4
c9c0 <s.ries@oth-aw.de>
2021-06-08 21:40:33 +0200

    Bugfixing Icon

--- sys-src/frontend/src/inputs/icontextfield.js
+++ sys-src/frontend/src/inputs/icontextfield.js
@@ -10,7 +10,7 @@ class IconTextField extends Component {
         return (
             <div style={styles.styleDiv} class="">
                 <div style={styles.styleIconDiv}>
-                    <span style={styles.styleSpan}><icon class={this.props.iconclass}/></span>
+                    <span style={styles.styleSpan}><i class={this.props.iconclass}/></span>
                 </div>
                 <input style={styles.styleInput} type={this.props.type} class="" placeholder={this.props.placeholder} aria-label="Username" aria-describedby="basic-addon1" onChange={ (e) => this.props.onChange(e.target.value)}/>
             </div>
