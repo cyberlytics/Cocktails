1a8f656aa4c47ee8d6321c0ace4a2b5fc269670a
c9c0 <s.ries@oth-aw.de>
2021-06-10 17:48:33 +0200

    Cocktail Element hinzugefügt

--- sys-src/frontend/src/components/List_Of_Cocktails.js
+++ sys-src/frontend/src/components/List_Of_Cocktails.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import CocktailsDataService from "../Service/cocktails"
-import {Link} from "react-router-dom"
+
+
+//Import own UI-Elements
+import Cocktail from './cocktail';
 
 const ListOfCocktails = props => {
   const [cocktails, setCocktails] = useState([])
@@ -22,22 +25,23 @@ const ListOfCocktails = props => {
         {
           cocktails.map((cocktail) => {
             return (
-              <div className="col-lg-4 pb-1">
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">{cocktail.name}</h5>
-                      {
-                        cocktail.ingredients.map( (ingredient, i) => (
-                          <div key={`ingredient-test-${i}`}>
-                            <b>{ingredient.name}</b> : {" "}
-                            {ingredient.quantity.quantity}
-                            {ingredient.quantity.unit}
-                          </div>
-                        ))
-                      }
-                  </div>
-                </div>
-              </div>
+              <Cocktail name={cocktail.name} ingredients={cocktail.ingredients}/>
+              //  <div className="col-lg-4 pb-1">
+              //   <div className="card">
+              //     <div className="card-body">
+              //       <h5 className="card-title">{cocktail.name}</h5>
+              //         {
+              //           cocktail.ingredients.map( (ingredient, i) => (
+              //             <div key={`ingredient-test-${i}`}>
+              //               <b>{ingredient.name}</b> : {" "}
+              //               {ingredient.quantity.quantity}
+              //               {ingredient.quantity.unit}
+              //             </div>
+              //           ))
+              //         }
+              //     </div>
+              //   </div>
+              // </div>
             );
           })
         }
