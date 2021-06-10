c5b303b46bcb77b2a8175a0ed4d97db6f8e1eeb6
Stefan Ries <s.ries@oth-aw.de>
2021-06-10 17:48:33 +0200

    Cocktail Element hinzugefügt

--- sys-src/frontend/src/components/favouriteslist.js
+++ sys-src/frontend/src/components/favouriteslist.js
@@ -41,22 +41,7 @@ function FavouritesList() {
       //TODO: Change this
       cocktails.map((cocktail) => {
         return (
-          <div className="col-lg-4 pb-1">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{cocktail.name}</h5>
-                  {
-                    cocktail.ingredients.map( (ingredient, i) => (
-                      <div key={`ingredient-test-${i}`}>
-                        <b>{ingredient.name}</b> : {" "}
-                        {ingredient.quantity.quantity}
-                        {ingredient.quantity.unit}
-                      </div>
-                    ))
-                  }
-              </div>
-            </div>
-          </div>
+          
         );
       })
     );
