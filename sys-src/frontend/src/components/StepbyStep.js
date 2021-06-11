beecbbea7778d226fb6ebc20de5f1a464aae8ba9
Michael <m.eichenseer1@oth-aw.de>
2021-06-11 15:31:53 +0200

    include pagination bring in pagination in the step by step view

--- sys-src/frontend/src/components/StepbyStep.js
+++ sys-src/frontend/src/components/StepbyStep.js
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+import Pagination from './Pagination'
+
 const StepbyStep = props => {
+    const[currentStep, setCurrentStep] = useState(props.data.steps[0]);
+
+    const paginate = (pageNumber) => setCurrentStep(pageNumber)
+
 
     return(
         <div>
-        <h1>Bild</h1>
-        <div>{props.data.name}</div>
+            <h1>Bild</h1>
+            <h2>{props.data.name}</h2>
+            <h3>{                        
+                props.data.ingredients.map( (ingredient, i) => (
+                <div key={`ingredient-test-${i}`}>
+                <b>{ingredient.name}</b> : {" "}
+                {ingredient.quantity.quantity}
+                {ingredient.quantity.unit}
+                </div>
+            ))
+            }
+            </h3>
+            <button>Start</button>
+
+            <ul className='list-group mb-4'>
+
+                        <li>
+                            {currentStep}
+                        </li>
+
+            </ul>
+
+            <Pagination steps={props.data.steps} paginate={paginate}></Pagination>
         </div>
     )
 }
