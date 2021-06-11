e113332b1116655842fc43e6a4cceaa404cbe56a
Michael <m.eichenseer1@oth-aw.de>
2021-06-11 13:17:11 +0200

    change href from banner

--- sys-src/frontend/src/TopContainer/topcontainer.js
+++ sys-src/frontend/src/TopContainer/topcontainer.js
@@ -12,7 +12,7 @@ class TopContainer extends Component {
         return (
             <nav class="navbar navbar-expand-lg navbar-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="/">
                         <Banner/>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
