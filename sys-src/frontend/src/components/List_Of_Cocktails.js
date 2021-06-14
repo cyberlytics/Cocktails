0ce9dd0059818e65eeb6eaed12f85892cdf0568a
Jakob Götz <j.goetz2@oth-aw.de>
2021-06-14 15:51:10 +0200

    cleaned List_Of_Cocktails.js file

--- sys-src/frontend/src/components/List_Of_Cocktails.js
+++ sys-src/frontend/src/components/List_Of_Cocktails.js
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import CocktailsDataService from "../Service/cocktails"
-import searchbarelement from "../TopContainer/NavigationBar/searchbarelement"
 
 //Import own UI-Elements
 import Cocktail from './cocktail';
@@ -9,16 +7,10 @@ const ListOfCocktails = props => {
   const [cocktails, setCocktails] = useState([])
 
   useEffect( () => {
-    retrieveCocktails();
+    
   }, [])
 
-  const retrieveCocktails = () => {
-    CocktailsDataService.getAll()
-      .then(response => {
-        console.log(response.data);
-        setCocktails(response.data)
-      })
-  }
+
 
     return (
       <div className="row">
