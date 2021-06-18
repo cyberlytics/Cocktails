f03663a15451e6eae0afb6e6a8072bb49194e50a
Stefan Ries <s.ries@oth-aw.de>
2021-06-18 03:26:21 +0200

    Registrierung + Favorisierbutton

--- sys-src/frontend/src/App.js
+++ sys-src/frontend/src/App.js
@@ -9,6 +9,7 @@ import BottomContainer from './BottomContainer/bottomcontainer';
 
 //Import local ressources
 import { apiurl } from './api';
+import cocktails from './Service/cocktails';
 
 class App extends Component {
     state = {
@@ -16,16 +17,83 @@ class App extends Component {
         cocktails : [],
     }
 
-    //Function to pass to childs, which enables them to set a state property
-    setValue(property, val) {
-        this.setState({
-            [property] : val
-        })
+    retrieveCocktails() {
+        CocktailsDataService.getAll()
+          .then(response => {
+            this.setState({cocktails: response.data})
+          })
     }
 
     getSearch(val){
-        this.setState({cocktails: val});
-        console.log(this.state.cocktails);
+        //Get array of favourited cocktail names
+        var favouritedCocktails = this.state.cocktails.filter(cocktail => cocktail.favourite).map(cocktail => cocktail.name);
+        //Add the favourite tag accordingly to all search result
+        var newCocktails = val.map(cocktail => ({...cocktail, favourite: favouritedCocktails.includes(cocktail.name)}));
+        this.setState({cocktails: newCocktails});
+    }
+
+    getFavourites() {
+        async function fetchData(self) {
+            let userid = localStorage.getItem('isLoggedInId');
+            try {
+                let res = await fetch(apiurl + '/user/' + userid + '/favourites', {
+                    method: "get",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                });
+                
+                //process result
+                let result = await res.json();
+
+                //Request successfull
+                if (result && result.success) {
+                    self.setState({
+                        //Add favourite field to every existing cocktail and set its value according to the fetch result
+                        cocktails: self.state.cocktails.map(cocktail => ({...cocktail, favourite: result.cocktails.map(favcocktail => favcocktail.name).includes(cocktail.name)})),
+                    });
+                }
+                //Request failed
+                else if (result && result.success === false) {
+                  self.setState({
+                    favouritedCocktails : null,
+                  })
+                }
+              } catch (error) {
+                  console.log(error.message);
+              }
+            }
+        fetchData(this);
+    }
+
+    getUserIsLoggedIn() {
+        async function fetchData(self) {
+            try {
+                const isLoggedInId = localStorage.getItem('isLoggedInId');
+                let res = await fetch(apiurl + '/login/' + isLoggedInId, {
+                method: 'get',
+                headers: {
+                'Accept' : 'application/json',
+                'Content-Type': 'application/json'
+                }
+            });
+        
+            let result = await res.json();
+            let isLoggedIn = result.success;
+        
+            if (isLoggedIn) {
+                self.setState({userIsLoggedIn : true});
+            }
+            else {
+                self.setState({userIsLoggedIn : false});
+            }
+            }
+            catch(e) {
+                console.log(e.message);
+            }
+        }
+        fetchData(this);
     }
 
     async handleLogout() {
@@ -62,46 +130,67 @@ class App extends Component {
         window.location.href='/';
     }
 
-    async componentDidMount() {
-        this.retrieveCocktails();
-        try {
-            const isLoggedInId = localStorage.getItem('isLoggedInId');
-            let res = await fetch(apiurl + '/login/' + isLoggedInId, {
-            method: 'get',
-            headers: {
-              'Accept' : 'application/json',
-              'Content-Type': 'application/json'
-            }
-          });
-    
-          let result = await res.json();
-          let isLoggedIn = result.success;
-    
-          if (isLoggedIn) {
-            this.setState({userIsLoggedIn : true});
-          }
-          else {
-            this.setState({userIsLoggedIn : false});
-          }
-        }
-        catch(e) {
-            console.log(e.message);
-        }
+    getCocktailId(cocktailname) {
+        return this.state.cocktails.find(cocktail => cocktail.name === cocktailname)._id;
     }
-    
-      retrieveCocktails() {
-        CocktailsDataService.getAll()
-          .then(response => {
-            this.setState({cocktails: response.data})
-          })
-      }
+
+    toggleFavourite(favname) {
+        async function toggle(self) {
+            await self.setState({
+                cocktails: self.state.cocktails.map(cocktail => ({...cocktail, favourite: cocktail.name === favname ? !cocktail.favourite : cocktail.favourite})),
+            });
+            self.getCocktailId(favname);
+            let userid = localStorage.getItem('isLoggedInId');
+            if(userid !== null) {
+                let cocktailid = self.getCocktailId(favname);
+                try {
+                    let res = await fetch(apiurl + '/user/setFavourite', {
+                        method: "post",
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            userid: userid, 
+                            cocktail: cocktailid,
+                        })
+                    });
+                    
+                    //process result
+                    let result = await res.json();
+        
+                    //Successful authenticated
+                    if (result && result.success) {
+
+                    }
+                    else if (result && result.success === false) {
+
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        toggle(this);
+    }
+
+    async componentDidMount() {
+        //Get Cocktails
+        this.retrieveCocktails();
+
+        //Get favourited cocktails
+        this.getFavourites();
+
+        //Check if user is already logged in
+        this.getUserIsLoggedIn();
+    }
 
 
     render() {
         return (
             <div>
                 <TopContainer userIsLoggedIn={this.state.userIsLoggedIn} onSearchFiltered={(val) => this.getSearch(val)} onLogout={(val) => this.handleLogout(val)}/>
-                <MainContainer userIsLoggedIn={this.state.userIsLoggedIn} cocktails={(this.state.cocktails)}/>
+                <MainContainer userIsLoggedIn={this.state.userIsLoggedIn} cocktails={(this.state.cocktails)} toggleFavourite={(val) =>this.toggleFavourite(val)}/>
                 <BottomContainer/>
             </div>
         );
--- sys-src/frontend/src/MainContainer/RegisterForm/registerform.js
+++ sys-src/frontend/src/MainContainer/RegisterForm/registerform.js
@@ -2,13 +2,13 @@
 import React, { Component } from 'react';
 
 //Import own UI-Elements
-import IconTextField from '../inputs/icontextfield';
+import IconTextField from '../../inputs/icontextfield';
 
 //Import local ressources
 import { styles } from './styles';
-import { apiurl } from '../api.js';
+import { apiurl } from '../../api';
 
-class LoginForm extends Component {
+class RegisterForm extends Component {
     
     //Initialize username and password field with empty string and enable button
     constructor(props) {
@@ -16,21 +16,24 @@ class LoginForm extends Component {
         this.state = {
             username: '',
             password: '',
+            passwordValidation: '',
             buttonDisabled: false
         };
         this.onKeyUp = this.handleEnter.bind(this);
+    }
+
+    componentDidMount() {
+        this.resetForm();
     }
 
     resetForm() {
         this.setState({
             username: '',
             password: '',
+            passwordValidation: '',
             buttonDisabled: false
         })
-    }
-
-    componentDidMount() {
-        this.resetForm();
+        console.log(this.state);
     }
 
     setInputValue(property, val) {
@@ -49,6 +52,9 @@ class LoginForm extends Component {
         if (!this.state.username) {
             return;
         }
+        if (!this.state.passwordValidation) {
+            return;
+        }
         if (!this.state.password) {
             return;
         }
@@ -58,7 +64,7 @@ class LoginForm extends Component {
         })
 
         try {
-            let res = await fetch(apiurl + '/login', {
+            let res = await fetch(apiurl + '/user/register', {
                 method: "post",
                 headers: {
                     'Accept': 'application/json',
@@ -66,25 +72,26 @@ class LoginForm extends Component {
                 },
                 body: JSON.stringify({
                     username: this.state.username, 
-                    password: this.state.password
+                    password: this.state.password,
+                    passwordValidation: this.state.passwordValidation,
                 })
             });
             
             //process result
             let result = await res.json();
 
-            //Successful authenticated
+            //Request successful
             if (result && result.success) {
-                localStorage.setItem('isLoggedInId', result.id);
-                window.location.href='/';
+                alert("Registrierung erfolgreich");
+                window.location.href='/login';
             }
             else if (result && result.success === false) {
-                //TODO: USER BENACHRICHTIGEN
-                this.resetForm();
+                this.setState({buttonDisabled: false});
+                alert(result.msg);
             }
         } catch (error) {
-            console.log(error.message);
             this.resetForm();
+            console.log(error.message);
         }
     }
 
@@ -97,17 +104,22 @@ class LoginForm extends Component {
     render() { 
         return (
             <div style={styles.styleDiv} className="login-form" onKeyPress={this.handleKeypress}>
-                <h1 style={styles.styleHeadline}>Login</h1>
+                <h1 style={styles.styleHeadline}>Register</h1>
+
                 <div style={styles.styleInputField}><IconTextField type="username" placeholder="Username" iconclass="fas fa-user"
                 value={this.state.username ? this.state.username : ''} onChange={ (val) => this.setInputValue('username', val)} /></div>
+                
                 <div style={styles.styleInputField}><IconTextField type="password" placeholder="Password" iconclass="fas fa-lock"
                 value={this.state.password ? this.state.password : ''} onChange={ (val) => this.setInputValue('password', val)} /></div>
+                
+                <div style={styles.styleInputField}><IconTextField type="password" placeholder="Password Validation" iconclass="fas fa-lock"
+                value={this.state.passwordValidation ? this.state.passwordValidation : ''} onChange={ (val) => this.setInputValue('passwordValidation', val)} /></div>
+                
                 <div style={styles.styleInputField}><button style={styles.styleButton} variant="contained" type="submit" class="btn btn-primary"
-                onClick={() => this.handleSubmit() } disabled={this.state.buttonDisabled}>Login</button></div>
-                <div style={styles.styleLinkText}><span>Not registered yet? Click <a href="/register">Here</a></span></div>
+                onClick={() => this.handleSubmit() } disabled={this.state.buttonDisabled}>Submit</button></div>
             </div>
         );
     }
 }
  
-export default LoginForm;
+export default RegisterForm;
