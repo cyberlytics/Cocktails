ec253e74e15e4924d77d73dfc41c8b97d5680131
Michael Eichenseer <m.eichenseer1@oth-aw.de>
2021-06-21 15:22:51 +0200

    make navbar sticky

--- sys-src/frontend/src/MainContainer/Cocktailoverview/cocktail.js
+++ sys-src/frontend/src/MainContainer/Cocktailoverview/cocktail.js
@@ -31,10 +31,10 @@ function Cocktail(props) {
 
         <li key={props.name} className="col-lg-4 pb-1 btn" onClick= {event => outerContainerHandler(event)} id={"OuterContainer"}>
             <div className="card border-dark mb-3 cocktailcontainer">
-            <h5 className="card-title fw-bold">{props.name}</h5>
+            <h5 className="card-title fw-bold ">{props.name}</h5>
             <div className="float-end m-0 btn btn-link"  onClick={event => innerContainerHandler(event)} id={"InnerContainer"}><i className={props.isFavourite ? "fa fa-star" : "far fa-star"}/></div>
             
-            <img src={`data:image/png;base64,${props.image}`} className="card-img-top"/>     
+            <img src={`data:image/png;base64,${props.image}`} className="card-img-bottom"/>     
                 <div class="overlay">
                     {
                         props.ingredients.map( (ingredient, i) => (
