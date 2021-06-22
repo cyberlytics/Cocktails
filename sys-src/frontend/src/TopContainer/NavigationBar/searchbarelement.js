b57e2bc9fffa540fb7aa086efefa8b18030c0384
Jak330 <jakob_goetz@yahoo.de>
2021-06-22 17:27:18 +0200

    Search function optimized. Need to change App.js in regards of the search.

--- sys-src/frontend/src/TopContainer/NavigationBar/searchbarelement.js
+++ sys-src/frontend/src/TopContainer/NavigationBar/searchbarelement.js
@@ -1,31 +1,19 @@
 //Import modules
 import React, { Component, useState, useEffect } from 'react';
-import CocktailsDataService from "../../Service/cocktails"
+
 
 const SearchBar = props => {
-    const [searchCocktail, setSearchCocktail] = useState('');
-    const [cocktails, setCocktails] = useState([])
 
     const onChangeSearchCocktail = e => {
         const searchCocktail = e.target.value; 
-        setSearchCocktail(searchCocktail)
+
+        const cocktails = props.cocktails;
 
         const filteredCocktails = cocktails.filter(cocktail => {
             return cocktail.name.toLowerCase().includes( searchCocktail.toLowerCase())
         })
         props.onSearchFiltered(filteredCocktails)
     };
-
-    useEffect( () => {
-      setCocktails(props.cocktails);
-    }, [])
-  
-    const retrieveCocktails = () => {
-      CocktailsDataService.getAll()
-        .then(response => {
-          setCocktails(response.data)
-        })
-    }
 
             return (
             <div>
