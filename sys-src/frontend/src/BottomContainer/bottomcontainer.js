7222e8eddb38a15abecfb59dcbb75db83a20636b
Jak330 <jakob_goetz@yahoo.de>
2021-06-26 20:15:09 +0200

    implementet Datenschutz button

--- sys-src/frontend/src/BottomContainer/bottomcontainer.js
+++ sys-src/frontend/src/BottomContainer/bottomcontainer.js
@@ -3,14 +3,20 @@ import React, { Component } from 'react';
 
 //Import own UI-Elements
 import Impressum from './BottomBar/impressumelement';
+import Datenschutz from './BottomBar/datenschutzelement'
 
 class BottomContainer extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
-            <Impressum></Impressum>
+            <div class='row g-0'>
+                <div class='btn-group col-md-5 w-auto ms-auto'>
+                    <Impressum value={"Impressum"} href="#" redirect={event => window.location.href='/Impressum'}></Impressum>
+                    <Datenschutz value={"Datenschutz"} href="#" redirect={event => window.location.href='/Datenschutz'}></Datenschutz>
+                </div>
+            </div>
         );
     }
 }
- 
+
 export default BottomContainer;
