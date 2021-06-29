b6a85e0efe1187190d66acd0aa489976e2c45d39
Michael <m.eichenseer1@oth-aw.de>
2021-06-29 15:55:55 +0200

    switch currsteps from stepbystep to pagination.js

--- sys-src/frontend/src/MainContainer/StepbyStep/Pagination.js
+++ sys-src/frontend/src/MainContainer/StepbyStep/Pagination.js
@@ -1,26 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 import "./style.css"
 
 const Pagination = props => {
+const[currentStep, setCurrentStep] = useState(0);
+
+const paginate = (stepNumber) => setCurrentStep(stepNumber)
+
+const increasestep = () => currentStep < (props.steps.length)-1 ? setCurrentStep(currentStep+1) : setCurrentStep(currentStep)
+const decreasestep = () => currentStep > 0 ? setCurrentStep(currentStep-1) : setCurrentStep(currentStep)
 
     return(
         <nav>
             <ul className="pagination">
-                <button onClick={()=> props.decreasestep()} className="page-link btn goldenbtn text-dark">Zurück</button>
+                <button onClick={()=> decreasestep()} className="page-link btn goldenbtn text-dark">Zurück</button>
 
                 {
 
                     props.steps.map( (number,i) => (
-                        <li key={i} className={i===props.currentStep? "activated":""}>
-                            <button onClick={()=> props.paginate(i)} className={i===props.currentStep? "page-link btn text-dark activated":"page-link btn goldenbtn text-dark"}>
+                        <li key={i} className={i===currentStep? "activated":""}>
+                            <button onClick={()=> paginate(i)} className={i===currentStep? "page-link btn text-dark activated":"page-link btn goldenbtn text-dark"}>
                                 {i+1}
                             </button>
                         </li>
                     ))
                 }
 
-                <button onClick={()=> props.increasestep()} className="page-link btn goldenbtn text-dark">Weiter</button>
+                <button onClick={()=> increasestep()} className="page-link btn goldenbtn text-dark">Weiter</button>
 
+            </ul>
+
+            <ul className='list-group mb-4'>
+                <div className="text-white">
+                    {props.steps[currentStep]}
+                </div>
             </ul>
         </nav>
     );
