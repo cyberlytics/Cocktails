c3a42c2ed644c694c9d1c4f6961bbc9281afc421
Jak330 <jakob_goetz@yahoo.de>
2021-06-29 03:17:32 +0200

    continued integrate tests. mainly rendering tests for the NavBar. Tests for button click at the NavBar are not working yet.

--- sys-src/frontend/src/TopContainer/NavigationBar/loginelement.test.js
+++ sys-src/frontend/src/TopContainer/NavigationBar/loginelement.test.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import LoginElement from './loginelement'
 import LoginForm from '../../LoginForm/loginform.js'
 
+/*
+//  Test for render Login and Logout correctly
+*/
 test("render Login correctly", () => {
     const { queryByText } = render(<LoginElement />)
     const button = queryByText("Login")
     expect(button).toBeTruthy()
 })
 
+test("render Logout correctly", () => {
+    const { queryByText } = render(<LoginElement userIsLoggedIn={true}/>)
+    expect(queryByText("Logout")).toBeTruthy()
+})
+
+/*
+//  Test for click Login and Logout correctly
+*/
 describe("click Login", () =>{
     it("onClick", () => {
         const { queryByText } = render(<LoginElement />)
@@ -17,11 +28,6 @@ describe("click Login", () =>{
         expect(LoginForm).toBeTruthy();
     }) 
 })
-
-    test("render Logout correctly", () => {
-        const { queryByText } = render(<LoginElement userIsLoggedIn={true}/>)
-        expect(queryByText("Logout")).toBeTruthy()
-    })
 
     describe("click Logout", () =>{
         it("onClick", () => {
