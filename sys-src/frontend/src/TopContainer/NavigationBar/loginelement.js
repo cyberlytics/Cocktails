a9c5c878b75547e05a56e537b5c97c1c588c31b5
Jak330 <jakob_goetz@yahoo.de>
2021-06-30 14:24:45 +0200

    corrected style import

--- sys-src/frontend/src/TopContainer/NavigationBar/loginelement.js
+++ sys-src/frontend/src/TopContainer/NavigationBar/loginelement.js
@@ -12,15 +12,15 @@ class LoginElement extends Component {
     render() {
         if (!this.props.userIsLoggedIn) {
             return (
-                <div style={styles.styleDiv}>
-                    <button data-testid="LoginButton" className="btn btn-outline-primary" onClick={this.props.redirect}>Login</button>
+                <div className="styleDiv" data-testid="LoginButton">
+                    <button className="btn btn-outline-primary" onClick={this.props.redirect}>Login</button>
                 </div>
             );
         }
         else {
             return (
                 <div className="styleDiv">
-                    <button className="btn navbutton" onClick={this.props.onLogout}>Logout</button>
+                    <button className="btn btn-outline-primary" onClick={this.props.onLogout}>Logout</button>
                 </div>
             );   
         }
