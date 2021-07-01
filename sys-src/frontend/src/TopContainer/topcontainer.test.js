0d896f1e724880c56cb68bf9cae5bd983c85a2fc
Jak330 <jakob_goetz@yahoo.de>
2021-07-01 13:01:30 +0200

    included searchbar test in topcontainer throw out unused includes in stepbystep testing

--- sys-src/frontend/src/TopContainer/topcontainer.test.js
+++ sys-src/frontend/src/TopContainer/topcontainer.test.js
@@ -9,3 +9,11 @@ test("logged out: click Login in TopContainer", () =>{
     expect(render(<LoginForm/>)).toBeTruthy();
 })
 
+test("handle onChange event", () => {
+    const searchCocktail = jest.fn((value) => {})
+    const cocktailList = [{name: 'WhiteRussian'}, {name: 'Mojito'}]
+    render(<TopContainer value="Search" tempcocktails={cocktailList} onSearchFiltered={searchCocktail}/>)
+    const input = screen.getByPlaceholderText("Suche")
+    fireEvent.change(input, {target: {value: "WhiteRussian"}})
+    expect(input.value).toBe("WhiteRussian")
+  })
