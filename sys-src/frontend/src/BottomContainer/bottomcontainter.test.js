450339dbbc0691f00cadf989a3d189cc776e471f
Jak330 <jakob_goetz@yahoo.de>
2021-07-02 13:55:56 +0200

    finished implementing bottomcontainer with testing and styling

--- sys-src/frontend/src/BottomContainer/bottomcontainter.test.js
+++ sys-src/frontend/src/BottomContainer/bottomcontainter.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import BottomContainer from './bottomcontainer'
-import Datenschutz from '../MainContainer/Datenschutz/datenschutz'
-import Impressum from '../MainContainer/Impressum/impressum'
 import '@testing-library/jest-dom/extend-expect';
 
 test("render Impressum in BottomContainer correctly", () => {
@@ -16,13 +14,3 @@ test("render Datenschutz in BottomContainer correctly", () => {
     const button = screen.getByText("Datenschutz")
     expect(button).toBeTruthy()
 })
-/*
-test("click Datenschutz in BottomContainer", () =>{
-    render(<BottomContainer/>)
-    fireEvent.click(screen.getByText("Datenschutz"))
-    expect(render(<Datenschutz/>)).toBeTruthy()
-})
-
-//const datenschutzerklärung = screen.getByText("Datenschutzerklärung").toBeInTheDocument() 
-// expect(screen.getByText("Sahne leicht anschlagen, so dass sie noch fließt und nicht steif ist")).toBeInTheDocument()  
-*/
